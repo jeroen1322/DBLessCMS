ff8910091c809471a1f9f8c9805dc559c01ddfcf
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-05-31 18:52:46 +0200

    Added CSS
    Made it prettier and easier to look at.

--- index.php
+++ index.php
@@ -1,7 +1,43 @@
 <?php
     $string=file_get_contents("articles.json");
     $json = json_decode($string, true);
+?>
 
-    foreach($json as $i){
-        echo "<strong>".$i['name'].":</strong> <br>".$i['article']."<br><br>";
-    }
+<head>
+    <title>DBLessCMS</title>
+    <style>
+        
+        body{
+            background-color: #C7FFE3;    
+            font-family: Arial;
+            height: 100%;
+        }
+        
+        #background{
+            max-width: 650px;
+            position: relative;
+            margin: 0 auto;
+            background-color: #E0FFF0;
+            height: 100%;
+            padding-top: 5px;
+            padding-left: 15px;
+            padding-right: 15px;
+            border-left: 1px solid grey;
+            border-right: 1px solid grey;
+        }
+        #article h1{
+            color: #686966;    
+            margin-bottom: 2px;
+        }
+    </style>
+</head>
+<body>
+    <div id="background">
+        <?php
+            foreach($json as $i){
+                echo "<div id='article'><h1>".$i['name'].":</h1>"."<i>".date('d.m.Y')."</i><p>".$i['article']."</p><hr><br></div>";
+            }
+        ?>
+    </div>
+</body>
+</html>
