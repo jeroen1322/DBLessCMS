e0ad35f32c5f0cb486eb235f3b32c6a585f2bd0a
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-05-31 17:27:40 +0200

    Made it work.

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<html>
-    <head>
-        <title>DBLessCMS</title>
-    </head>
-    <body>
-    <? php
-        $string=file_get_contents("articles.json");
-        $text=json_decode($string, true);
-        $article=$text['articles'];
-        for($text as $wauw){
-            echo $wauw['name']." ".$wauw['article'];
-        }
-    ?>
-    </body>
-</html>
+<?php
+    $string=file_get_contents("articles.json");
+    $json = json_decode($string, true);
+
+    foreach($json as $i){
+        echo "<strong>".$i['name'].":</strong> <br>".$i['article']."<br><br>";
+    }
